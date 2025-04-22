94e2994a557d9504f178222e737993148522cfb5
Kamule <benjamin.herbomez@gmail.com>
2025-04-22 11:21:51 +0200

    Add logs

--- stream-bingo-backend/src/grid/services/grid/grid.service.ts
+++ stream-bingo-backend/src/grid/services/grid/grid.service.ts
@@ -10,84 +10,85 @@ import { v7 as uuid } from 'uuid'
 
 @Injectable()
 export class GridService {
-    constructor(
-        @InjectRepository(GridEntity)
-        private readonly gridRepository: Repository<GridEntity>,
-        private readonly cellService: CellService,
-        private readonly roundService: RoundService,
-    ){}
+  constructor(
+    @InjectRepository(GridEntity)
+    private readonly gridRepository: Repository<GridEntity>,
+    private readonly cellService: CellService,
+    private readonly roundService: RoundService,
+  ) { }
 
-    async getGridForStream(streamId: string, userId?: string, bingoId?: string): Promise<GridEntity | null> {
-      
-        let where = {}
-        if(userId != null && bingoId != null){
-            where = {
-                user: { id : userId },
-                id: bingoId,
-            }
-        }
-        else if(userId != null || bingoId != null){
-            const round = await this.roundService.getStreamCurrentRound(streamId)
-            if(round?.id == null){
-                return null
-            }
-            where = {
-                round: { id: round.id },
-                user: userId ? {id: userId } : IsNull(),
-                ...(bingoId != null ? {id: bingoId} : {})
-            }
-        }
-        else{
-            throw Error('No bingo to show')
-        }
-        return this.gridRepository.findOne({
-            where:  where,
-            relations: ['round', 'round.stream', 'cells', 'cells.cell'],
-        })
-    }
+  async getGridForStream(streamId: string, userId?: string, bingoId?: string): Promise<GridEntity | null> {
 
-    async createGrid(streamId: string, userId?: string): Promise<GridEntity> {
-        const availableCells = await this.cellService.getStreamCells(streamId)
-        let cellIds = availableCells.map(({id}) => id)
-        const round = await this.roundService.getStreamCurrentRound(streamId)
-        if(round == null){
-            throw new Error('No active round')
-        }
-        const gridId = uuid()
-        const cells: Array<DeepPartial<GridCellEntity>> = Array.from(Array(16).keys()).map(index => {
-            const cellId = cellIds.at(Math.floor(Math.random()*cellIds.length))!
-            cellIds = cellIds.filter(id => id != cellId)
-            return {
-                index,
-                cellId,
-                gridId,
-            }
-        })
-        await  this.gridRepository.save({
-            id: gridId,
-            locked: false,
-            round: {id: round.id },
-            cells,
-            ...userId ? { user: { id: userId }}: {}
-        }, {
-            transaction: true
-        })
-        return this.gridRepository.findOne({
-            where: { id: gridId},
-            relations: ['round', 'round.stream', 'cells', 'cells.cell']
-        }) as Promise<GridEntity>
+    let where = {}
+    console.log(userId, bingoId)
+    if (userId != null && bingoId != null) {
+      where = {
+        user: { id: userId },
+        id: bingoId,
+      }
     }
+    else if (userId != null || bingoId != null) {
+      const round = await this.roundService.getStreamCurrentRound(streamId)
+      if (round?.id == null) {
+        return null
+      }
+      where = {
+        round: { id: round.id },
+        user: userId ? { id: userId } : IsNull(),
+        ...(bingoId != null ? { id: bingoId } : {})
+      }
+    }
+    else {
+      throw Error('No bingo to show')
+    }
+    return this.gridRepository.findOne({
+      where: where,
+      relations: ['round', 'round.stream', 'cells', 'cells.cell'],
+    })
+  }
 
-    getUserGrids(userId: string, query: PaginateQuery): Promise<Paginated<GridEntity>> {
-      return paginate(
-        query, 
-        this.gridRepository,
-        {
-            where: {
-                user: { id : userId}
-            },
-            relations: ['round', 'round.stream'],
-            sortableColumns: ['round.name', 'round.stream.name']
-        })
+  async createGrid(streamId: string, userId?: string): Promise<GridEntity> {
+    const availableCells = await this.cellService.getStreamCells(streamId)
+    let cellIds = availableCells.map(({ id }) => id)
+    const round = await this.roundService.getStreamCurrentRound(streamId)
+    if (round == null) {
+      throw new Error('No active round')
     }
+    const gridId = uuid()
+    const cells: Array<DeepPartial<GridCellEntity>> = Array.from(Array(16).keys()).map(index => {
+      const cellId = cellIds.at(Math.floor(Math.random() * cellIds.length))!
+      cellIds = cellIds.filter(id => id != cellId)
+      return {
+        index,
+        cellId,
+        gridId,
+      }
+    })
+    await this.gridRepository.save({
+      id: gridId,
+      locked: false,
+      round: { id: round.id },
+      cells,
+      ...userId ? { user: { id: userId } } : {}
+    }, {
+      transaction: true
+    })
+    return this.gridRepository.findOne({
+      where: { id: gridId },
+      relations: ['round', 'round.stream', 'cells', 'cells.cell']
+    }) as Promise<GridEntity>
+  }
+
+  getUserGrids(userId: string, query: PaginateQuery): Promise<Paginated<GridEntity>> {
+    return paginate(
+      query,
+      this.gridRepository,
+      {
+        where: {
+          user: { id: userId }
+        },
+        relations: ['round', 'round.stream'],
+        sortableColumns: ['round.name', 'round.stream.name']
+      })
+  }
 }
